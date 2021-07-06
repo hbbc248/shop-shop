6b157b5b2193caa34dc55e8a6bf3d4cb623cd552
Ibrahim <ibrahimzerlin@hotmail.com>
2021-07-06 09:40:38 -0500

    done with indexedDb

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
 
 const Cart = () => {
 
@@ -21,6 +22,19 @@ const Cart = () => {
         });
         return sum.toFixed(2);
     }
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
+
 
     if (!state.cartOpen) {
         return (
--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -5,24 +5,36 @@ import { QUERY_CATEGORIES } from '../../utils/queries';
 
 import { useStoreContext } from "../../utils/GlobalState";
 
+// indexedDB
+import { idbPromise } from '../../utils/helpers';
+
 function CategoryMenu() {
 
   const [state, dispatch] = useStoreContext();
 
   const { categories } = state;
 
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  // added "loading" for indexedDB
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
-    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
     if (categoryData) {
-      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories
       });
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        });
+      });
     }
-  }, [categoryData, dispatch]);
+  }, [categoryData, loading, dispatch]);
 
   const handleClick = id => {
     dispatch({
